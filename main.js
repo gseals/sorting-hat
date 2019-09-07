b823afc6bcf62279460267a1f9c0a233ede24397
Gabriel Seals <sealsgj@gmail.com>
2019-09-07 14:20:57 -0500

    updating index and css and js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+//array of items to be randomly printed
+
 const houses =["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"];
+
+//printer prints items once
 
 const printer = (toPrint, divId) => {
   document.getElementById(divId).innerHTML = toPrint
@@ -16,10 +20,12 @@ const sortingPrinter = () => {
     <label for="studentName" class="sr-only">Student name</label>
     <input type="text" class="form-control" id="studentName" placeholder="Name here, please">
   </div>
-  <button type="button" class="btn btn-primary mb-2" id="sort" onclick="cardPrinting()">Sort!</button>
+  <button type="button" class="btn btn-primary mb-2" id="sort">Sort!</button>
   </form>`
   printer(string, 'sorting');
 }
+
+//printToDom because one should print more than once, while the above prints once
 
 const printToDom = (toPrint, divId) => {
   document.getElementById(divId).innerHTML += toPrint
@@ -28,7 +34,7 @@ const printToDom = (toPrint, divId) => {
 let i = 0;
 
 const cardPrinting = (arr) => {
-    let houseName = arr[Math.floor(Math.random() * houses.length)];
+    let houseName = arr[Math.floor(Math.random() * arr.length)];
     let nameValue = document.getElementById('studentName').value;
     let domString = '';
     domString += `
@@ -37,12 +43,14 @@ const cardPrinting = (arr) => {
       <h5 class="card-title">${nameValue}</h5>
       <h6 class="card-subtitle mb-2 text-muted">You'll fit well in ${houseName}!</h6>
       <p class="card-text">Congratulations! Don't die!</p>
-      <button value="${i}" class="card-link">Expel</button>
+      <button value="${i}" class="card-link" id="expel">Expel</button>
     </div>
   </div>`
   i += 1;
   printToDom(domString, 'random-card');
 }
+
+//below: a global button click listener
 
 document.body.addEventListener('click', (e) => {
     if (e.target.id === 'sortButton') {
@@ -51,10 +59,13 @@ document.body.addEventListener('click', (e) => {
     if (e.target.id === 'sort') {
       cardPrinting(houses);
     }
+    if (e.target.id === 'expel') {
+      const cardValue = e.target.value;
+      const cardId = `card-${cardValue}`;
+      const card = document.getElementById(cardId);
+      card.parentNode.removeChild(card);
+    }
+  })
 
-})
-
-//these are the buttons that trigger the above functions
-
-// document.getElementById('sortButton').addEventListener('click', sortingPrinter);
-// document.getElementById('sort').addEventListener('click', cardPrinting);   this doesn't work. why?????
+// expel button;
+// clear field;
